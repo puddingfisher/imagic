4bf48b4ecb45759d1a3b36850057c10fbb6dfb76
puddingfisher <54524642+puddingfisher@users.noreply.github.com>
2024-04-25 11:46:57 +0800

    Upload by PicGo

---
+++ img/202404251146665.js
@@ -0,0 +1,468 @@
+var data={
+    openType: 11,//网维环境下打开浏览器的方式
+    /*第一行左上轮播图*/
+    slide: {
+        areaName: '轮播',//广告位名称
+        list:[
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'lunbo-1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '1统帅小队FPS'
+            },
+            {
+                imgUrl:'media/3.png',
+                name:'lunbo-1',
+                isSet: 0,
+                barId: '23',
+                gameLink: 'https://www.baidu.com//?text=1',
+                detail: '2统帅小队FPS'
+            },
+            {
+                imgUrl:'media/1.png',
+                name:'lunbo-1',
+                isSet: 0,
+                barId: '23',
+                gameLink: 'https://www.baidu.com//?text=1' ,
+                detail: '3统帅小队小队FPS战争网游3统帅小队小队FPS战争网游'
+            },
+            {
+                imgUrl:'media/1.png',
+                name:'lunbo-1',
+                isSet: 0,
+                barId: '23',
+                gameLink: 'https://www.baidu.com//?text=1' ,
+                detail: '4统帅小队FPS战'
+            },
+            {
+                imgUrl:'media/3.png',
+                name:'lunbo-1',
+                isSet: 0,
+                barId: '23',
+                gameLink: 'https://www.baidu.com//?text=1',
+                detail: '5统帅小队FPS战'
+            }
+        ]
+    },
+    /*第一行左下*/
+    recommend:{
+        areaName: '人气',//广告位名称
+        list:[
+            {
+                imgUrl:'media/1.jpg',//图片链接
+                name:'recommend-1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1' //跳转链接
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'recommend-1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1' //跳转链接
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'recommend-1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1' //跳转链接
+            }
+        ]
+    },
+    /*第一行右侧*/
+    hotOnline:{
+        areaName: '热门网游',//广告位名称
+        list:[
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-1',//游戏名字
+                mark:0,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-2',//游戏名字
+                mark:1,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-3',//游戏名字
+                mark:2,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','英雄']//多个标签用英文,隔开
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-4',//游戏名字
+                mark:3,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-5',//游戏名字
+                mark: 0,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-6',//游戏名字
+                mark:2,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','英雄']//多个标签用英文,隔开
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-7',//游戏名字
+                mark:3,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+            },
+            {
+                imgUrl:'media/2.png',//图片链接
+                name:'大富翁-8',//游戏名字
+                mark: 0,//左上角标签 0：无；1：热门；2：最新；3：推荐
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+            }
+        ]
+    },
+    /*第二行排行榜1*/
+    rank1:{
+        areaName: '热门端游1',//广告位名称
+        moreLink: 'https://www.baidu.com/',//更多链接
+        list:[
+            {
+                imgUrl:'media/1.png',//图片链接
+                imgSUrl:'media/6.png',//第一行小图
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            }
+        ]
+    },
+    /*第二行排行榜2*/
+    rank2:{
+        areaName: '热门端游2',//广告位名称
+        moreLink: '',//更多链接
+        list:[
+            {
+                imgUrl:'media/1.png',//图片链接
+                imgSUrl:'media/6.png',//第一行小图
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            }
+        ]
+    },
+    /*第二行排行榜3*/
+    rank3:{
+        areaName: '热门端游3',//广告位名称
+        moreLink: '',//更多链接
+        list:[
+            {
+                imgUrl:'media/1.png',//图片链接
+                imgSUrl:'media/6.png',//第一行小图
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 1,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            }
+        ]
+    },
+    /*第二行排行榜4*/
+    rank4:{
+        areaName: '热门端游4',//广告位名称
+        moreLink: '',//更多链接
+        list:[
+            {
+                imgUrl:'media/1.png',//图片链接
+                imgSUrl:'media/6.png',//第一行小图
+                name:'游戏1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            },
+            {
+                imgUrl:'media/1.png',//图片链接
+                name:'游戏1',//游戏名字
+                isSet: 0,//0：默认优先启端，1：默认启链接
+                barId: '23',//启端id
+                gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                detail: '新版本新皮肤'
+            }
+        ]
+    },
+    /*第三行横板图*/
+    panel:{
+        areaName: '横板图',//广告位名称
+        imgUrl:'media/3.png',//图片链接
+        name:'横板图',//游戏名字
+        isSet: 0,//0：默认优先启端，1：默认启链接
+        barId: '23',//启端id
+        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+    },
+    /*游戏库*/
+    library:{
+        areaName: '游戏库',//广告位名称
+        list:[
+            {
+                type: '端游',
+                group:[
+                    {
+                        imgUrl:'media/1.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "1点击时刻，发现更多精彩"
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    }
+                ]
+            },
+            {
+                type: '页游',
+                group:[
+                    {
+                        imgUrl:'media/1.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "2点击时刻，发现更多精彩"
+                    },
+                    {
+                        imgUrl:'media/3.png',//图片链接
+                        name:'游戏1',//游戏名字
+                        isSet: 0,//0：默认优先启端，1：默认启链接
+                        barId: '23',//启端id
+                        gameLink: 'https://www.baidu.com//?text=1', //跳转链接
+                        detail: "点击时刻，发现更多精彩",
+                        lbt:['角色扮演','科幻','英雄']//多个标签用英文,隔开
+                    }
+                ]
+            }
+        ]
+    }
+}
